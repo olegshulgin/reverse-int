d383f166a1bda36da3ee28ac382074f64ba8692c
olegshulgin <97721233+olegshulgin@users.noreply.github.com>
2022-06-28 10:44:40 +0300

    feat: update index.js file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function reverse (n) {
     let result = 0;
-    result = Math.abs(parseInt(String(num).split("").reverse().join(""), 10));
+    result = Math.abs(parseInt(String(n).split("").reverse().join(""), 10));
     return result;
 }
